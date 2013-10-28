859ba4fd2d463242e114710a948f40cf19eea12f
kruemel <soeren@siegismund-poschmann.de>
2013-10-28 11:30:32 +0100

    Commit

--- Easy/Curl/Curl.php
+++ Easy/Curl/Curl.php
@@ -6,10 +6,13 @@ class Curl
 	{
 		$this->ckfile = tempnam ("/tmp", "CURLCOOKIE");
 	}
-	public function get($url)
+	public function get($url, $getCookie=false)
 	{
 		$ch = curl_init ($url);
-		curl_setopt ($ch, CURLOPT_COOKIEJAR, $this->ckfile);
+		if($getCookie)
+		{
+			curl_setopt ($ch, CURLOPT_COOKIEJAR, $this->ckfile);
+		}
 		curl_setopt ($ch, CURLOPT_COOKIEFILE, $this->ckfile);
 		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
 		return curl_exec($ch);
