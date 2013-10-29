dd063729a138401e833720c53769cd1add465d00
kruemel <soeren@siegismund-poschmann.de>
2013-10-29 17:37:46 +0100

    CURL

--- Easy/Curl/Curl.php
+++ Easy/Curl/Curl.php
@@ -25,5 +25,15 @@ class Curl
 		curl_close($ch);
 		return $r;
 	}
+	public function post($url, $param)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_POST,1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+		$result=curl_exec ($ch);
+		curl_close ($ch);
+		return $result;
+	}
 }
 ?>
